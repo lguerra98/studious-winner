1c7168c6358e7dd20f1f778e03ba89be05f3c06a
3132183450Cps. <carolinapineda0504@gmail.com>
2025-06-15 14:34:21 -0500

    Ajuste dashboard

--- cancel_py.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Users\Lukas\anaconda3\envs\HR\python.exe" "C:\Users\Lukas\Desktop\studious-winner\cancel.py"
--- run_py.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Users\Lukas\anaconda3\envs\HR\python.exe" "C:\Users\Lukas\Desktop\studious-winner\run.py"
-pause
